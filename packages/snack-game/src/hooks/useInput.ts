a657760ef47247303480690868aaae3f90f3b013
dev-dong-su <fkdlrj1230@gmail.com>
2023-07-11 13:58:08 +0900

    feat: useForm, useInput 추가

--- packages/snack-game/src/hooks/useInput.ts
+++ packages/snack-game/src/hooks/useInput.ts
@@ -6,20 +6,23 @@ interface useInputProps<T> {
 }
 
 const useInput = <T>({ initialValue, isInvalid }: useInputProps<T>) => {
+  const [valid, setValid] = useState<boolean>(true);
   const [value, setValue] = useState(initialValue);
 
   const handleChangeValue = (event: ChangeEvent<HTMLInputElement>): void => {
     const eventValue = event.target.value as unknown as T;
 
-    if (isInvalid && isInvalid(eventValue)) return;
+    if (isInvalid) {
+      setValid(isInvalid(eventValue));
+    }
 
-    console.log(eventValue);
     setValue(eventValue);
   };
 
   return {
     value,
     handleChangeValue,
+    valid,
   };
 };
 
