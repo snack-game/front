9db8a31d71023fc88c717accbf355be4ddbe9052
dev-dong-su <fkdlrj1230@gmail.com>
2023-09-14 20:02:31 +0900

    feat: modal 준비

--- packages/snack-game/src/components/common/Modal/Modal.tsx
+++ packages/snack-game/src/components/common/Modal/Modal.tsx
@@ -1,9 +1,26 @@
-interface ModalProps {
-  children?: never;
-}
+import useModal from '@hooks/useModal';
 
-const Modal = ({ children }: ModalProps) => {
-  return <div></div>;
+const Modal = () => {
+  const { toggleModal, isShowing } = useModal();
+
+  const handleClose = () => {
+    toggleModal();
+  };
+
+  return (
+    <>
+      {isShowing && (
+        <div
+          css={{
+            width: '100vw',
+            height: '100vh',
+            position: 'fixed',
+            top: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            zIndex: '10',
+          }}
+        ></div>
+      )}
+    </>
+  );
 };
-
-export default Modal;
