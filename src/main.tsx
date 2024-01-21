39b0b02178407f32f531c615e6f8dd99693bbcbd
dev-dong-su <fkdlrj1230@gmail.com>
2024-01-21 14:09:13 +0900

    fix: ga 초기 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
+import ReactGA from 'react-ga4';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
@@ -22,6 +23,15 @@ const queryClient = new QueryClient({
 });
 
 const Root = () => {
+  useEffect(() => {
+    ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
+  }, []);
+
+  ReactGA.send({
+    hitType: 'pageview',
+    page: location.pathname,
+  });
+
   return (
     <React.StrictMode>
       <HelmetProvider>
