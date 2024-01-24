81aad2d0ad8c9c23aa0c6bca7fcd87cebfd3b8e3
Hee Su <fkdlrj1230@gmail.com>
2024-01-24 11:10:54 +0900

    feat: header 반응형으로 수정 (#118)
    * feat: header 반응형으로 수정
    
    * 유저 프로필을 nav 상단으로
    
    Co-authored-by: 이유진 <youjin6325@naver.com>
    
    ---------
    
    Co-authored-by: 이유진 <youjin6325@naver.com>

--- src/pages/games/AppleGame/components/AppleGameHeader.tsx
+++ src/pages/games/AppleGame/components/AppleGameHeader.tsx
@@ -4,31 +4,24 @@ import GameIcon from '@assets/icon/game.svg?react';
 import RankingIcon from '@assets/icon/ranking.svg?react';
 import Header from '@components/Header/Header';
 import RouterLink from '@components/RouterLink/RouterLink';
-import Spacing from '@components/Spacing/Spacing';
 
 import PATH from '@constants/path.constant';
 
 const AppleGameHeader = () => {
-  return <Header nav={<HeaderNav />} />;
-};
-
-const HeaderNav = () => {
   const location = useLocation().pathname;
 
-  console.log(PATH.APPLE_GAME);
   return (
-    <div className={'flex'}>
-      <RouterLink
-        to={PATH.APPLE_GAME}
-        className={'items-center'}
-        isActivated={location == PATH.APPLE_GAME}
-      >
-        <GameIcon className={'mr-4 h-6 w-6'} />
-        <span>게임</span>
-      </RouterLink>
-
-      <Spacing size={3} direction={'horizontal'} />
-
+    <Header>
+      <Header.ListItem>
+        <RouterLink
+          to={PATH.APPLE_GAME}
+          className={'items-center'}
+          isActivated={location == PATH.APPLE_GAME}
+        >
+          <GameIcon className={'mr-4 h-6 w-6'} />
+          <span>게임</span>
+        </RouterLink>
+      </Header.ListItem>
       <RouterLink
         to={PATH.APPLE_GAME_RANKING}
         className={'items-center'}
@@ -37,7 +30,7 @@ const HeaderNav = () => {
         <RankingIcon className={'mr-4 h-6 w-6'} />
         <span>랭킹</span>
       </RouterLink>
-    </div>
+    </Header>
   );
 };
 
