20593a1a217e1ad971a2c7a4a5f0f5617d2a1257
nijuy <youjin6325@naver.com>
2024-02-05 19:58:56 +0900

    feat: 프로필 편집 중일 때 화면 요소 추가

--- src/pages/user/components/UserInfo.tsx
+++ src/pages/user/components/UserInfo.tsx
@@ -1,17 +1,39 @@
+import { useState } from 'react';
+
 import Spacing from '@components/Spacing/Spacing';
 
 import HistoryChart from './HistoryChart';
 import Profile from './Profile';
 
 const UserInfo = () => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const onClickEdit = () => {
+    setIsEditing(true);
+  };
+
+  const onClickDone = () => {
+    setIsEditing(false);
+    // 프로필 수정 요청
+  };
+
+  const onClickClose = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div
       className={`relative mx-auto flex w-full max-w-7xl flex-col items-center`}
     >
       <div className={`h-52 w-full bg-rose-100`}></div>
-      <Profile />
+      <Profile
+        isEditing={isEditing}
+        onClickEdit={onClickEdit}
+        onClickDone={onClickDone}
+        onClickClose={onClickClose}
+      />
       <Spacing size={16} />
-      <HistoryChart />
+      {!isEditing && <HistoryChart />}
     </div>
   );
 };
