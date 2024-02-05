428f9cc106aabd283a28b468a4349c766456be1f
nijuy <youjin6325@naver.com>
2024-02-05 19:59:27 +0900

    fix: UserPage 컴포넌트 헤더 AppleGameHeader로 교체
    - 불필요한 Spacing도 같이 삭제

--- src/pages/user/UserPage.tsx
+++ src/pages/user/UserPage.tsx
@@ -3,8 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import QueryBoundary from '@components/base/QueryBoundary';
 import RetryError from '@components/Error/RetryError';
 import Footer from '@components/Footer/Footer';
-import Header from '@components/Header/Header';
-import Spacing from '@components/Spacing/Spacing';
+import AppleGameHeader from '@pages/games/AppleGame/components/AppleGameHeader';
 
 import UserInfo from './components/UserInfo';
 
@@ -14,8 +13,7 @@ const UserPage = () => {
       <Helmet>
         <title>Snack Game || My Info</title>
       </Helmet>
-      <Header className={'fixed'} />
-      <Spacing size={4} />
+      <AppleGameHeader />
       <div className="flex flex-col">
         <QueryBoundary errorFallback={RetryError}>
           <UserInfo />
