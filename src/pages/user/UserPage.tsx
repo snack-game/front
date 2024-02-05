70f7f200137127602155965b08b68a3f7c871d91
nijuy <youjin6325@naver.com>
2024-02-05 16:02:49 +0900

    feat: UserInfo, UserPage 컴포넌트 생성
    - 기본적인 화면 레이아웃만 잡은 상태

---
+++ src/pages/user/UserPage.tsx
@@ -0,0 +1,29 @@
+import { Helmet } from 'react-helmet-async';
+
+import QueryBoundary from '@components/base/QueryBoundary';
+import RetryError from '@components/Error/RetryError';
+import Footer from '@components/Footer/Footer';
+import Header from '@components/Header/Header';
+import Spacing from '@components/Spacing/Spacing';
+
+import UserInfo from './components/UserInfo';
+
+const UserPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Snack Game || My Info</title>
+      </Helmet>
+      <Header className={'fixed'} />
+      <Spacing size={4} />
+      <div className="flex flex-col">
+        <QueryBoundary errorFallback={RetryError}>
+          <UserInfo />
+        </QueryBoundary>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default UserPage;
---
+++ src/pages/user/components/UserInfo.tsx
@@ -0,0 +1,9 @@
+const UserInfo = () => {
+  return (
+    <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
+      마이페이지
+    </div>
+  );
+};
+
+export default UserInfo;
