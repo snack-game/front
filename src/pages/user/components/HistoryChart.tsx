a8ba98700670900bf55ec62248707c5b1e017718
nijuy <youjin6325@naver.com>
2024-02-05 21:26:18 +0900

    feat: HistoryChart 컴포넌트 탭 기능 추가
    - 탭 이름은 임시로 넣어둠

--- src/pages/user/components/HistoryChart.tsx
+++ src/pages/user/components/HistoryChart.tsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+
 const HistoryChart = () => {
+  const [currentTab, setCurrentTab] = useState(0);
+  const tabOption = ['탭 0', '탭 1'];
+
+  const onClick = (tabIndex: number) => {
+    setCurrentTab(tabIndex);
+  };
+
   return (
     <div className={`h-56 w-4/5`}>
-      <span className={`text-lg`}> 탭 1 </span>
-      <span className={`text-lg`}> 탭 2 </span>
-      <div className={`mt-2 h-52 w-full rounded-md bg-game`}></div>
+      <div className={`flex gap-2`}>
+        {tabOption.map((tab, index) => (
+          <span
+            className={`cursor-pointer text-lg ${
+              currentTab === index ? 'text-primary' : 'text-[#6B7280]'
+            }`}
+            key={tab}
+            onClick={() => {
+              onClick(index);
+            }}
+          >
+            {tab}
+          </span>
+        ))}
+      </div>
+
+      <div className={`mt-2 h-52 w-full rounded-md bg-game`}>
+        {currentTab}번째 탭 조회중
+      </div>
     </div>
   );
 };
