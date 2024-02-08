87a61637441351effab36e3514fb998f61bb7d04
nijuy <youjin6325@naver.com>
2024-02-08 16:33:55 +0900

    feat: UserPage 컴포넌트 헤더 Header로 교체
    - 사과게임이 아니라 스낵게임의 마이페이지니까 Header가 맞음 진진짜로

--- src/pages/user/UserPage.tsx
+++ src/pages/user/UserPage.tsx
@@ -3,7 +3,8 @@ import { Helmet } from 'react-helmet-async';
 import QueryBoundary from '@components/base/QueryBoundary';
 import RetryError from '@components/Error/RetryError';
 import Footer from '@components/Footer/Footer';
-import AppleGameHeader from '@pages/games/AppleGame/components/AppleGameHeader';
+import Header from '@components/Header/Header';
+import Spacing from '@components/Spacing/Spacing';
 import UserInfo from '@pages/user/components/UserInfo';
 
 const UserPage = () => {
@@ -12,7 +13,8 @@ const UserPage = () => {
       <Helmet>
         <title>Snack Game || My Info</title>
       </Helmet>
-      <AppleGameHeader />
+      <Header className={'fixed'} />
+      <Spacing size={4} />
       <div className="flex flex-col">
         <QueryBoundary errorFallback={RetryError}>
           <UserInfo />
