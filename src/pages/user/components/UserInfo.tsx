146a919e69aa5289c553c1d4f8adb0c667ea5ab4
nijuy <youjin6325@naver.com>
2024-03-25 11:18:40 +0900

    feat: 마이 페이지 로그아웃 버튼 추가

--- src/pages/user/components/UserInfo.tsx
+++ src/pages/user/components/UserInfo.tsx
@@ -1,15 +1,31 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import { useResetRecoilState } from 'recoil';
+
+import authApi from '@api/auth.api';
 import Spacing from '@components/Spacing/Spacing';
 import ChartSection from '@pages/user/components/ChartSection';
 import GuestToMember from '@pages/user/components/GuestToMember';
 import ProfileSection from '@pages/user/components/ProfileSection';
+import { resetUserState } from '@utils/atoms/member.atom';
 
+import { LOCAL_STORAGE_KEY } from '@constants/localStorage.constant';
+import PATH from '@constants/path.constant';
 import { useGetMemberProfile } from '@hooks/queries/members.query';
+import useLocalStorage from '@hooks/useLocalStorage';
+import useToast from '@hooks/useToast';
 
 const UserInfo = () => {
   const profile = useGetMemberProfile();
   const [isEditing, setIsEditing] = useState(false);
+
+  const openToast = useToast();
+  const resetUser = useResetRecoilState(resetUserState);
+  const navigate = useNavigate();
+  const { deleteStorageValue } = useLocalStorage({
+    key: LOCAL_STORAGE_KEY.USER_EXPIRE_TIME,
+  });
 
   const handleClickEdit = () => {
     setIsEditing(true);
@@ -23,10 +39,24 @@ const UserInfo = () => {
     setIsEditing(false);
   };
 
+  const handleLogout = async () => {
+    resetUser();
+    await authApi.logOut();
+    openToast('로그아웃 성공!', 'success');
+    deleteStorageValue();
+    navigate(PATH.MAIN, { replace: true });
+  };
+
   return (
     <div
       className={'relative mx-auto flex w-full max-w-7xl flex-col items-center'}
     >
+      <button
+        className="absolute right-0 top-44 z-10 mr-2 text-gray-400"
+        onClick={handleLogout}
+      >
+        로그아웃
+      </button>
       <div
         className={
           'mt-52 flex h-fit w-full flex-col items-center rounded-xl bg-game pb-24'
