32d704a6150943413be9c0b1a8240c6bbfcc42d6
nijuy <youjin6325@naver.com>
2024-04-07 02:47:17 +0900

    feat: `GameResult` 컴포넌트에 `ExpBarChart` 추가
    - 게임 종료 후의 status를 얻기 위해 프로필 쿼리를 한번 날린다

--- src/pages/games/AppleGame/components/GameResult.tsx
+++ src/pages/games/AppleGame/components/GameResult.tsx
@@ -4,15 +4,22 @@ import OAuth from '@components/Auth/OAuth';
 import Button from '@components/Button/Button';
 import { userState } from '@utils/atoms/member.atom';
 
-import { useIntegrateMember } from '@hooks/queries/members.query';
+import {
+  useGetMemberProfile,
+  useIntegrateMember,
+} from '@hooks/queries/members.query';
 import useModal from '@hooks/useModal';
+
+import ExpBarChart from './ExpBarChart';
 
 interface GameResultProps {
   score: number;
+  percentile?: number;
   reStart: () => void;
 }
 
-const GameResult = ({ score, reStart }: GameResultProps) => {
+const GameResult = ({ score, percentile, reStart }: GameResultProps) => {
+  const profile = useGetMemberProfile();
   const userStateValue = useRecoilValue(userState);
   const integrateMember = useIntegrateMember();
   const { closeModal } = useModal();
@@ -28,8 +35,16 @@ const GameResult = ({ score, reStart }: GameResultProps) => {
 
   return (
     <div className={'flex h-full w-full flex-col justify-evenly'}>
-      <div className={'mx-auto flex flex-col gap-4 font-semibold'}>
-        <p>최종점수: {score}점!</p>
+      <div className={'mx-auto flex flex-col items-center gap-4 font-semibold'}>
+        <p>최종점수: {score}점</p>
+
+        {userStateValue.type !== 'GUEST' && (
+          <>
+            {percentile && <p>전체 사용자 중 상위 {percentile}%의 점수에요!</p>}
+            {profile.status && <ExpBarChart status={profile.status} />}
+          </>
+        )}
+
         <Button onClick={handleReStartButton} size={'lg'}>
           재시작!
         </Button>
