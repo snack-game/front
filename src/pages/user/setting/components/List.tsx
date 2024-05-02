3498431433d63ebb91185bbb2a22407e17eb4244
nijuy <youjin6325@naver.com>
2024-05-02 22:59:21 +0900

    chore: 불필요한 태그 삭제
    - hr 삭제하면서 같이 지웠어야 했는데 깜빡쓰

--- src/pages/user/setting/components/List.tsx
+++ src/pages/user/setting/components/List.tsx
@@ -20,12 +20,10 @@ const ListRoot = ({
   children: React.ReactNode;
 }) => {
   return (
-    <>
-      <div className="border-b-[1px] border-gray-200 px-4 py-3">
-        <div className="text-md flex h-10 items-center">{title}</div>
-        <ul className="flex flex-col gap-4"> {children} </ul>
-      </div>
-    </>
+    <div className="border-b-[1px] border-gray-200 px-4 py-3">
+      <div className="text-md flex h-10 items-center">{title}</div>
+      <ul className="flex flex-col gap-4"> {children} </ul>
+    </div>
   );
 };
 
