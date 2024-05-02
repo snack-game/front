565becf2ef42309138cbc83568120f692b01c83d
nijuy <youjin6325@naver.com>
2024-05-02 21:48:30 +0900

    refactor: `UserInfo` 로그아웃 로직 삭제, 설정 버튼 추가
    - 로그아웃은 설정 페이지로 이동

--- src/pages/user/components/UserInfo.tsx
+++ src/pages/user/components/UserInfo.tsx
@@ -1,31 +1,18 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-import { useResetRecoilState } from 'recoil';
-
-import authApi from '@api/auth.api';
+import SettingIcon from '@assets/icon/settings.svg?react';
+import RouterLink from '@components/RouterLink/RouterLink';
 import Spacing from '@components/Spacing/Spacing';
 import ChartSection from '@pages/user/components/ChartSection';
 import GuestToMember from '@pages/user/components/GuestToMember';
 import ProfileSection from '@pages/user/components/ProfileSection';
-import { resetUserState } from '@utils/atoms/member.atom';
 
-import { LOCAL_STORAGE_KEY } from '@constants/localStorage.constant';
 import PATH from '@constants/path.constant';
 import { useGetMemberProfile } from '@hooks/queries/members.query';
-import useLocalStorage from '@hooks/useLocalStorage';
-import useToast from '@hooks/useToast';
 
 const UserInfo = () => {
   const profile = useGetMemberProfile();
   const [isEditing, setIsEditing] = useState(false);
-
-  const openToast = useToast();
-  const resetUser = useResetRecoilState(resetUserState);
-  const navigate = useNavigate();
-  const { deleteStorageValue } = useLocalStorage({
-    key: LOCAL_STORAGE_KEY.USER_EXPIRE_TIME,
-  });
 
   const handleClickEdit = () => {
     setIsEditing(true);
@@ -39,22 +26,11 @@ const UserInfo = () => {
     setIsEditing(false);
   };
 
-  const handleLogout = async () => {
-    resetUser();
-    await authApi.logOut();
-    openToast('로그아웃 성공!', 'success');
-    deleteStorageValue();
-    navigate(PATH.MAIN, { replace: true });
-  };
-
   return (
     <div className="relative mt-20 h-full pt-20">
-      <button
-        className="absolute right-0 top-14 z-10 mr-2 text-gray-400"
-        onClick={handleLogout}
-      >
-        로그아웃
-      </button>
+      <RouterLink to={PATH.SETTING}>
+        <SettingIcon className="absolute right-0 top-12 z-10 mr-2" />
+      </RouterLink>
       <div className={'flex min-h-full flex-col items-center bg-game pb-20'}>
         {profile && (
           <ProfileSection
