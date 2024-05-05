2b7144822ef863e6ed7aeeb9c0db0004a7a192b0
nijuy <youjin6325@naver.com>
2024-05-05 16:46:58 +0900

    chore: 불필요한 import문 삭제

--- src/pages/games/SnackGame/ranking/RankingPage.tsx
+++ src/pages/games/SnackGame/ranking/RankingPage.tsx
@@ -2,9 +2,7 @@ import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import Dropdown, { DropDownOptionType } from '@components/DropDown/DropDown';
-import Footer from '@components/Footer/Footer';
 import Spacing from '@components/Spacing/Spacing';
-import AppleGameHeader from '@pages/games/SnackGame/components/AppleGameHeader';
 import RankingSection from '@pages/games/SnackGame/ranking/components/RankingSection';
 
 import { useGetSeasons } from '@hooks/queries/ranking.query';
