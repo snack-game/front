7b75995bec265342a862165aa50697d4caa72ec9
dev-dong-su <fkdlrj1230@gmail.com>
2024-07-01 11:03:37 +0900

    feat: 에러 바운더리 reset 동작 개선

--- src/pages/games/SnackGame/game/SnackGamePage.tsx
+++ src/pages/games/SnackGame/game/SnackGamePage.tsx
@@ -1,11 +1,24 @@
 import { Helmet } from 'react-helmet-async';
 
+import { useRecoilValue } from 'recoil';
+
 import ErrorBoundary from '@components/base/ErrorBoundary';
 import retryError from '@components/Error/RetryError';
+import { pixiState } from '@utils/atoms/game.atom';
 
+import { LobbyScreen } from './screen/LobbyScreen';
 import SnackGameBase from './SnackGameBase';
+import { navigation } from './util/navigation';
 
 const SnackGamePage = () => {
+  const pixiValue = useRecoilValue(pixiState);
+
+  const handleRetryGameError = () => {
+    if (pixiValue.assetsInit && pixiValue.pixiInit) {
+      navigation.showScreen(LobbyScreen);
+    }
+  };
+
   return (
     <>
       <Helmet>
@@ -13,7 +26,7 @@ const SnackGamePage = () => {
       </Helmet>
 
       <div className={'h-screen bg-game'}>
-        <ErrorBoundary fallback={retryError}>
+        <ErrorBoundary fallback={retryError} onReset={handleRetryGameError}>
           <SnackGameBase />
         </ErrorBoundary>
       </div>
