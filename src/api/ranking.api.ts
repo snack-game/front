88cb7acad5d1d5f22395610f2ec0f8d3e22028c2
nijuy <youjin6325@naver.com>
2024-07-02 00:32:17 +0900

    refactor: ranking api 요청 url 생성 방식 수정

--- src/api/ranking.api.ts
+++ src/api/ranking.api.ts
@@ -7,48 +7,48 @@ import {
 
 const rankingApi = {
   endPoint: {
-    totalRanking: '/rankings',
-    userRanking: '/rankings',
+    ranking: ({
+      gameId,
+      season,
+      user = false,
+    }: {
+      gameId: RankingViewType;
+      season?: number;
+      user?: boolean;
+    }) => {
+      const seasonPath = season ? `/${season}` : '';
+      const userPath = user ? '/me' : '';
 
-    seasonRanking: '/rankings',
-    seasonRankingMe: '/rankings',
-
+      return `/rankings/${gameId}${seasonPath}${userPath}?by=BEST_SCORE`;
+    },
     seasons: '/seasons',
   },
 
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-
   totalRanking: async (gameId: RankingViewType) => {
-    const { data } = await api.get(
-      `${rankingApi.endPoint.totalRanking}/${gameId}?by=BEST_SCORE`,
-    );
+    const { data } = await api.get(rankingApi.endPoint.ranking({ gameId }));
     return data;
   },
 
   userRanking: async (gameId: RankingViewType) => {
     const { data } = await api.get(
-      `${rankingApi.endPoint.userRanking}/${gameId}/me?by=BEST_SCORE`,
+      rankingApi.endPoint.ranking({ gameId, user: true }),
     );
     return data;
   },
 
-  // TODO: season -> game 이 아니라 game -> season 순서로 변경 필요 (테스트 후 마지막으로 변경 예정)
   seasonRanking: async (
     season: number,
     gameId: RankingViewType,
   ): Promise<RankingType[]> => {
     const { data } = await api.get(
-      `${rankingApi.endPoint.seasonRanking}/${season}/${gameId}?by=BEST_SCORE`,
+      rankingApi.endPoint.ranking({ gameId, season }),
     );
     return data;
   },
 
   seasonRankingMe: async (season: number, gameId: RankingViewType) => {
     const { data } = await api.get(
-      `${rankingApi.endPoint.seasonRankingMe}/${season}/${gameId}/me?by=BEST_SCORE`,
+      rankingApi.endPoint.ranking({ gameId, season, user: true }),
     );
     return data;
   },
