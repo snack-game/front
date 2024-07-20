6067285dbd6c1b39cb60ce4229343c45c63ee21d
nijuy <youjin6325@naver.com>
2024-07-20 21:21:10 +0900

    design: `BottomNav` fixed -> sticky

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -11,7 +11,7 @@ export const BottomNav = () => {
   const location = useLocation().pathname;
 
   return (
-    <div className="fixed bottom-0 z-[999] flex w-full justify-center">
+    <div className="sticky bottom-0 z-[999] flex w-full justify-center">
       <div className="flex h-16 w-full max-w-3xl items-center justify-around rounded-t-2xl bg-primary-light text-gray-500 shadow-navigation">
         <RouterLink
           to={PATH.SNACK_GAME}
