a041b3fc50d5444517874da7796d3e1e925d4d84
0chil <0@chll.it>
2024-07-25 01:23:32 +0900

    feat: 앱에 토큰 재발급을 위임할 수 있다 (#264)
    * feat: 앱에 토큰 재발급을 위임할 수 있다
    
    * fix(axios.interceptor): 재발급 요청임을 미리 표시한다
    
    * typo: TOKEN_UNRESOLVEABLE_EXCEPTION
    
    * fix(axios.interceptor): 로컬 스토리지 클리어
    
    * typo: failureHandler
    
    Co-authored-by: 이유진 <youjin6325@naver.com>
    
    ---------
    
    Co-authored-by: 이유진 <youjin6325@naver.com>

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import PATH from '@constants/path.constant';
+
 import authApi from './auth.api';
 
 const api = axios.create({
@@ -16,22 +18,65 @@ api.interceptors.response.use(
       const status = error.response.status;
       const { code } = error.response.data;
       const originalRequest = error.config;
-      if (!originalRequest._retry) {
-        // _retry 플래그를 사용하여 무한 재시도 방지
-        originalRequest._retry = true;
 
-        if (status === 401 && code === 'TOKEN_EXPIRED_EXCEPTION') {
-          await authApi.tokenReIssue();
-          return api.request(originalRequest);
-        }
-        if (status === 401 && code === 'REFRESH_TOKEN_EXPIRED_EXCEPTION') {
+      if (status === 401 && code === 'TOKEN_EXPIRED_EXCEPTION') {
+        try {
+          if (window.navigator.userAgent.includes('SnackgameApp')) {
+            await appRefresh();
+          } else {
+            await authApi.tokenReIssue();
+            originalRequest._renewal = true;
+          }
+        } catch (error) {
           await authApi.logOut();
+          localStorage.clear();
+          window.dispatchEvent(new Event('loggedOut'));
+          window.location.href = PATH.MAIN;
         }
+        return api.request(originalRequest);
+      }
+      if (
+        originalRequest._renewal === true &&
+        status === 401 &&
+        (code === 'REFRESH_TOKEN_EXPIRED_EXCEPTION' ||
+          code === 'TOKEN_UNRESOLVABLE_EXCEPTION')
+      ) {
+        await authApi.logOut();
+        localStorage.clear();
+        window.dispatchEvent(new Event('loggedOut'));
+        window.location.href = PATH.MAIN;
       }
     }
     // 처리되지 않은 오류는 그대로 반환
     return Promise.reject(error);
   },
 );
+
+function appRefresh(): Promise<void> {
+  dispatchEvent(new CustomEvent('app-refresh-requested'));
+  return new Promise((resolve, reject) => {
+    const successHandler = () => {
+      cleanup();
+      resolve();
+    };
+    const failureHandler = () => {
+      cleanup();
+      reject(new Error('app logged out'));
+    };
+    window.addEventListener('app-refreshed', successHandler);
+    window.addEventListener('app-refresh-failed', failureHandler);
+
+    const timeout = setTimeout(() => {
+      cleanup();
+      reject(new Error('app refresh timeout'));
+    }, 5000);
+
+    const cleanup = () => {
+      window.removeEventListener('app-refreshed', successHandler);
+      window.removeEventListener('app-refresh-failed', failureHandler);
+      clearTimeout(timeout);
+    };
+  });
+}
 
 export default api;
