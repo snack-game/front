791cb264809b13d3ed63ee7e300e645ce5130575
0chil <0@chll.it>
2024-07-26 20:11:04 +0900

    style(GuestToMember): 유저 정보 레이아웃 수정

--- src/pages/user/components/GuestToMember.tsx
+++ src/pages/user/components/GuestToMember.tsx
@@ -47,17 +47,16 @@ const GuestToMember = () => {
   };
 
   return (
-    <div className="mb-8 w-4/5 rounded-md bg-white px-4 pb-12 pt-8 text-center">
-      <p className="whitespace-pre-line text-primary-deep-dark">
+    <div className="mx-4 rounded-md bg-white px-4 pb-8 pt-8 text-center md:w-4/5">
+      <OAuth oAuthOnSuccess={onOAuthSuccess} />
+      <p className="mt-8 whitespace-pre-line text-primary-deep-dark">
         {t('guest_to_member')}
       </p>
-      <div className="md:flex md:justify-around">
+      <div className="flex flex-wrap justify-evenly gap-2">
         {memberOnlyFeature.map((feature) => (
           <Feature key={feature.name} {...feature} />
         ))}
       </div>
-      <Spacing size={3} />
-      <OAuth oAuthOnSuccess={onOAuthSuccess} />
     </div>
   );
 };
@@ -72,7 +71,7 @@ const Feature = ({ svg, name, description }: FeatureProps) => {
   const { t } = useTranslation('user');
 
   return (
-    <div className="mt-8 flex flex-col items-center gap-4">
+    <div className="mt-8 flex basis-5/12 flex-col items-center gap-4 md:basis-2/12">
       <div className="text-primary">{svg}</div>
       <p className="text-lg font-semibold text-primary">{t(name)}</p>
       <p className="whitespace-pre-line text-center text-primary-deep-dark">
