7923017bb8884413a621425624a39c35141565f5
nijuy <youjin6325@naver.com>
2024-08-08 22:52:12 +0900

    chore: `GameLayout` 주석 삭제
    - App.tsx에 동일한 로직이 있음

--- src/pages/GameLayout.tsx
+++ src/pages/GameLayout.tsx
@@ -1,30 +1,8 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-
-import { useRecoilState } from 'recoil';
+import { Outlet } from 'react-router-dom';
 
 import { BottomNav } from '@components/BottomNav/BottomNav';
-import { userState } from '@utils/atoms/member.atom';
 
 const GameLayout = () => {
-  const navigate = useNavigate();
-  const [userStateValue, setUserState] = useRecoilState(userState);
-
-  // useEffect(() => {
-  //   const updateProfile = async () => {
-  //     const profile = await membersApi.getMemberProfile();
-  //     setUserState({ ...profile });
-  //   };
-
-  //   if (window.navigator.userAgent.includes('SnackgameApp')) {
-  //     updateProfile();
-  //     return;
-  //   }
-
-  //   if (!userStateValue.id) {
-  //     navigate(PATH.AUTH, { replace: true });
-  //   }
-  // }, []);
-
   return (
     <>
       <Outlet />
