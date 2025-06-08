de1c3047de0bcbcb76bde7665b3029b9509d046c
nijuy <youjin6325@naver.com>
2025-06-08 18:31:48 +0900

    chore: 최근 공지 저장용 localStorage key 추가

--- src/constants/localStorage.constant.ts
+++ src/constants/localStorage.constant.ts
@@ -11,3 +11,4 @@ export const KEY_PREFIX_STATS = 'stats-';
 export const KEY_PREFIX_BEST_SCORE = 'stats-best-score-';
 export const KEY_GAME_MODE = 'game-mode';
 export const KEY_GAME_SETTINGS = 'game-settings';
+export const KEY_LAST_NOTICE_INFO = 'lastNoticeInfo';
