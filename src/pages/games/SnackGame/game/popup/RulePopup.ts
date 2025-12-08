96a43673b7d119c39729d184ba733f8b04352c69
이유진 <youjin6325@naver.com>
2025-12-08 20:48:06 +0900

    refactor: 게임 팝업의 공통 코드를 BasePopup으로 분리 (#381)
    * feat: BasePopup 클래스 추가
    
    - Setting/Rule/PausePopup에서 공통되는 기능을 제공하는 클래스
    
    * refactor: 게임 내 팝업들이 BasePopup을 확장하도록 수정
    
    * remove(BasePopup): titleColor 제거
    
    - 0xf58529나 0xfb923c나 오렌지 계열 컬러라 전자로 통일

--- src/pages/games/SnackGame/game/popup/RulePopup.ts
+++ src/pages/games/SnackGame/game/popup/RulePopup.ts
@@ -1,43 +1,22 @@
-import gsap from 'gsap';
 import { t } from 'i18next';
-import { Container, Rectangle, Sprite, Text, Texture } from 'pixi.js';
+import { Sprite, Text } from 'pixi.js';
 
-import { AppScreen } from '../screen/appScreen';
+import { BasePopup } from './BasePopup';
 import { SnackgameApplication } from '../screen/SnackgameApplication';
 import { Label } from '../ui/Label';
 import { LargeButton } from '../ui/LargeButton';
-import { RoundedBox } from '../ui/RoundedBox';
 
 /** LobbyScreen에서 도움말을 클릭했을 때 표시되는 팝업 */
-export class RulePopup extends Container implements AppScreen {
-  private bg: Sprite;
-  private panel: Container;
-  private title: Label;
-  private panelBase: RoundedBox;
+export class RulePopup extends BasePopup {
   private matchImage: Sprite;
   private ruleText: Text;
   private doneButton: LargeButton;
 
-  constructor(private app: SnackgameApplication) {
-    super();
-
-    this.bg = new Sprite(Texture.WHITE);
-    this.bg.tint = 0xffedd5;
-    this.bg.interactive = true;
-    this.addChild(this.bg);
-
-    this.panel = new Container();
-    this.addChild(this.panel);
-
-    this.panelBase = new RoundedBox({ height: 480 });
-    this.panel.addChild(this.panelBase);
-
-    this.title = new Label(t('rule', { ns: 'game' }), {
-      fill: 0xf58529,
-      fontSize: 50,
+  constructor(app: SnackgameApplication) {
+    super(app, {
+      panelHeight: 480,
+      title: t('rule', { ns: 'game' }),
     });
-    this.title.y = -this.panelBase.boxHeight * 0.5 + 60;
-    this.panel.addChild(this.title);
 
     this.matchImage = Sprite.from('match');
     this.matchImage.anchor.set(0.5);
@@ -58,41 +37,7 @@ export class RulePopup extends Container implements AppScreen {
     this.panel.addChild(this.doneButton);
   }
 
-  async onPrepare(screen: Rectangle) {
-    // no-op
-  }
-
   public async handleDoneButton() {
     this.app.dismissPopup();
-  }
-
-  /** 창 크기가 변경될 때마다 호출되는 팝업 크기 조정 */
-  public onResize({ width, height }: Rectangle) {
-    this.bg.width = width;
-    this.bg.height = height;
-    this.panel.x = width * 0.5;
-    this.panel.y = height * 0.5;
-  }
-
-  /** 팝업을 애니메이션과 함께 표시 */
-  public async onShow({ width, height }: Rectangle) {
-    gsap.killTweensOf(this.bg);
-    gsap.killTweensOf(this.panel.pivot);
-    this.bg.alpha = 0;
-    this.panel.pivot.y = -400;
-    gsap.to(this.bg, { alpha: 0.8, duration: 0.2, ease: 'linear' });
-    await gsap.to(this.panel.pivot, { y: 0, duration: 0.3, ease: 'back.out' });
-  }
-
-  /** 팝업을 애니메이션과 함께 해제 */
-  public async onHide({ width, height }: Rectangle) {
-    gsap.killTweensOf(this.bg);
-    gsap.killTweensOf(this.panel.pivot);
-    gsap.to(this.bg, { alpha: 0, duration: 0.2, ease: 'linear' });
-    await gsap.to(this.panel.pivot, {
-      y: -500,
-      duration: 0.3,
-      ease: 'back.in',
-    });
   }
 }
