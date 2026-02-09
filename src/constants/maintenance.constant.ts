8ce3b6b52cbda6a023f003b4b980e482ade1b0b6
이유진 <youjin6325@naver.com>
2026-02-09 19:38:20 +0900

    chore: 피버타임 배포용 점검 시간 수정

--- src/constants/maintenance.constant.ts
+++ src/constants/maintenance.constant.ts
@@ -1,5 +1,5 @@
-export const MAINTENANCE_START = new Date('2026-02-01T00:00:00+09:00');
-export const MAINTENANCE_END = new Date('2026-02-01T23:59:00+09:00');
+export const MAINTENANCE_START = new Date('2026-02-09T21:00:00+09:00');
+export const MAINTENANCE_END = new Date('2026-02-09T23:59:00+09:00');
 
 export const isInMaintenance = (now = new Date()) => {
   return now >= MAINTENANCE_START && now <= MAINTENANCE_END;
