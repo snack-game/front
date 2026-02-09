0c868c2bc165fcd422b90365fabc6992cb378afa
nijuy <youjin6325@naver.com>
2026-02-09 20:03:50 +0900

    refactor(MaintenanceGuard): isWhitelisted 함수를 컴포넌트 외부로 이동

--- src/pages/MaintenanceGuard.tsx
+++ src/pages/MaintenanceGuard.tsx
@@ -1,17 +1,15 @@
 import { ReactNode } from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
+
 import { isInMaintenance } from '@constants/maintenance.constant';
 import PATH from '@constants/path.constant';
 
+const isWhitelisted = (pathname: string): boolean => {
+  return pathname === PATH.MAIN || pathname === PATH.MAINTENANCE;
+};
+
 export const MaintenanceGuard = ({ children }: { children: ReactNode }) => {
   const location = useLocation();
-
-  const isWhitelisted = (pathname: string): boolean => {
-    return (
-      pathname === PATH.MAIN ||
-      pathname === PATH.MAINTENANCE
-    );
-  };
 
   if (isInMaintenance() && !isWhitelisted(location.pathname)) {
     return <Navigate to={PATH.MAINTENANCE} replace />;
